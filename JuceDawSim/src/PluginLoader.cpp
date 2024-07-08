47948a601c642ed84ac39d054f1f723f27aef4ea
Tamas Polgar <polgar.timo@gmail.com>
2024-07-08 08:40:32 +0200

    Minor refactoring of PluginLoader in order to make it more general and less coupled to specific plugins and functions.

--- JuceDawSim/src/PluginLoader.cpp
+++ JuceDawSim/src/PluginLoader.cpp
@@ -3,20 +3,22 @@
 #include <wchar.h>
 
 
-PluginLoader::PluginLoader(const wchar_t* dllName, pul::Daw& daw)
+PluginLoader::PluginLoader(const wchar_t* dllName)
+    : PluginLoader(dllName, nullptr) { }
+
+PluginLoader::PluginLoader(const wchar_t* dllName, pul::Daw* daw)
 #ifdef _MSC_VER
     : m_hInstLib{ LoadLibrary(dllName) }, m_Daw{ daw }
 {
     if (!m_hInstLib)
     {
         std::wcerr << "Unable to load " << dllName << std::endl;
+        return;
     } 
-    else
+    if (!callFuncOnPlugin("AudioProcessorInit"))
     {
-        if (!callFuncOnPlugin("AudioProcessorInit"))
-        {
-            std::cerr << "Unable to initialize plugin.\n";
-        }
+        std::cerr << "Unable to initialize plugin.\n";
+        return;
     }
 
 }
@@ -34,8 +36,9 @@ PluginLoader::~PluginLoader()
 
         if (ProcAdd)
         {
+            ASSERT(m_Daw != nullptr);
             std::cout << "Finalising...\n";
-            ProcAdd(m_Daw);
+            ProcAdd(*m_Daw);
         }
         m_FreeResult = FreeLibrary(m_hInstLib);
         ASSERT(m_FreeResult);
@@ -44,16 +47,16 @@ PluginLoader::~PluginLoader()
 
 bool PluginLoader::callFuncOnPlugin(const char* funcName)
 {
-
     if (m_hInstLib)
     {
+        ASSERT(m_Daw != nullptr);
         MYPROC ProcAdd = (MYPROC)GetProcAddress(m_hInstLib, funcName);
-
+        
         // If the function address is valid, call the function.
         if (ProcAdd)
         {
             m_RunTimeLinkSuccess = true;
-            ProcAdd(m_Daw);
+            ProcAdd(*m_Daw);
             return true;
         }
         return false;
--- JuceDawSim/src/PluginLoader.h
+++ JuceDawSim/src/PluginLoader.h
@@ -12,14 +12,15 @@ class PluginLoader
 {
 public:
     using MYPROC = void(__cdecl*)(pul::Daw&);
-    explicit PluginLoader(const wchar_t* dllName, pul::Daw& daw);
+    explicit PluginLoader(const wchar_t* dllName);
+    PluginLoader(const wchar_t* dllName, pul::Daw* daw);
     PluginLoader(const PluginLoader&) = delete;
     ~PluginLoader();
     bool callFuncOnPlugin(const char* funcName);
     bool isLoadSuccessful() const noexcept { return m_RunTimeLinkSuccess; }
 private:
     HINSTANCE m_hInstLib{ nullptr };
-    pul::Daw& m_Daw;
+    pul::Daw* m_Daw{ nullptr };
     bool m_FreeResult{ false };
     bool m_RunTimeLinkSuccess{ false };
 
